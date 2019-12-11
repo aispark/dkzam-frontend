7fe370ca6c6378bac956532db02e0d5d3772dbdd
aispark <aispark@paran.com>
2019-12-11 15:35:30 +0900

    state 추가

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,28 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    bottomNav: "",
+    appBarTitle: "Delivery Order"
   },
   mutations: {
+    updateBottomNav(state, bottomNav) {
+      state.bottomNav = bottomNav;
+    },
+    updateAppBarTitle(state, appBarTitle) {
+      state.appBarTitle = appBarTitle;
+    }
   },
   actions: {
+    updateBottomNav({ commit }, payload) {
+      commit("updateBottomNav", payload);
+    },
+    updateAppBarTitle({ commit }, payload) {
+      commit("updateAppBarTitle", payload);
+    }
   },
-  modules: {
-  }
-})
+  modules: {}
+});
