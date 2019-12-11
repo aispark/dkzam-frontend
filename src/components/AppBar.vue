c510db0366d84dad0570e3d96a8a09770d446dbe
aispark <aispark@paran.com>
2019-12-11 15:33:28 +0900

    view에 따라 title이 변경 되도록 기능 추가

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app-bar :clipped-left="$vuetify.breakpoint.lgAndUp" app color="primary" dark>
-    <v-toolbar-title style="width: 300px" class="ml-0 pl-4">Delivery Order</v-toolbar-title>
+    <v-toolbar-title style="width: 300px" class="ml-0 pl-4">{{$store.state.appBarTitle}}</v-toolbar-title>
 
     <v-spacer />
     <v-btn icon>
