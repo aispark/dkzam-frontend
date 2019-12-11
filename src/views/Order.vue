b603634be6c20aea7eb8b8c517e34517cb8c2052
aispark <aispark@paran.com>
2019-12-11 15:36:07 +0900

    주문 목록 기능 추가

--- src/views/Order.vue
+++ src/views/Order.vue
@@ -15,7 +15,7 @@
           :loading="loading"
           :disabled="loading"
           @click="exportOrder"
-        >발주확인 완료</v-btn>
+        >발주확인 목록</v-btn>
         <v-btn
           class="ma-2"
           color="secondary"
@@ -27,7 +27,41 @@
     </v-row>
     <v-row align="center" justify="center">
       <v-col cols="12">
-        <ProgressMessage :items="processMessage" />
+        <ProgressMessage :items="processMessage" :isShow="isShow" :loading="loading" />
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col v-for="item in items" :key="item.productOrderId" cols="12" sm="6" md="4" lg="3">
+        <v-card>
+          <v-card-title class="subheading font-weight-bold">{{item.productOrderAddressName}}</v-card-title>
+          <v-divider></v-divider>
+          <v-list dense>
+            <v-list-item>
+              <v-list-item-content>수취인:</v-list-item-content>
+              <v-list-item-content class="align-end">{{item.productOrderAddressName}}</v-list-item-content>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-content>연락처:</v-list-item-content>
+              <v-list-item-content class="align-end">{{item.productOrderAddressTelno1}}</v-list-item-content>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-content>상품명:</v-list-item-content>
+              <v-list-item-content class="align-end">{{item.oproductOrderProductProductName}}</v-list-item-content>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-content>옵션:</v-list-item-content>
+              <v-list-item-content class="align-end">{{item.oproductOrderProductOptionContents}}</v-list-item-content>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-content>수량:</v-list-item-content>
+              <v-list-item-content class="align-end">{{item.productOrderDetailOrderQuantity}}</v-list-item-content>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-content>메모:</v-list-item-content>
+              <v-list-item-content class="align-end">{{item.productOrderMemo}}</v-list-item-content>
+            </v-list-item>
+          </v-list>
+        </v-card>
       </v-col>
     </v-row>
   </v-container>
@@ -39,20 +73,24 @@ export default {
   data() {
     return {
       loading: false,
-      processMessage: []
+      isShow: true,
+      processMessage: [],
+      items: []
     };
   },
   created() {
     // this.socketId = uuidv1();
-    console.log("process.env.VUE_APP_WEBSOCKET", process.env.VUE_APP_WEBSOCKET);
+    // console.log("process.env.VUE_APP_WEBSOCKET", process.env.VUE_APP_WEBSOCKET);
     this.$socket.on("orderProcess:log", data => {
       this.processMessage.push(data);
     });
   },
+  mounted() {},
   methods: {
     async placeOrder() {
       this.processMessage = [];
       this.loading = true;
+      this.isShow = true;
       const {
         data: { resultList }
       } = await this.$axios.get(
@@ -65,17 +103,41 @@ export default {
     async exportOrder() {
       this.processMessage = [];
       this.loading = true;
+      this.isShow = true;
       const {
         data: { resultList }
       } = await this.$axios.get(
         `${process.env.VUE_APP_WEBSOCKET}/smartStore/orderList`,
         { params: { socketId: this.$socket.id } }
       );
-      console.log(resultList);
+      this.items = resultList;
+      // this.items = [
+      //   {
+      //     productOrderId: 1,
+      //     productOrderAddressName: 2,
+      //     productOrderAddressTelno1: 3,
+      //     productOrderAddressTelno2: 3,
+      //     productOrderAddressZipcode: 3,
+      //     productOrderAddressAddress: 3,
+      //     oproductOrderProductProductName: 3,
+      //     oproductOrderProductOptionContents: 3,
+      //     productOrderDetailOrderQuantity: 3,
+      //     productOrderMemo: 3
+      //   }
+      // ];
       this.loading = false;
     },
 
-    alpsUploadOrder() {}
+    async alpsUploadOrder() {
+      this.processMessage = [];
+      this.loading = true;
+      this.isShow = true;
+      await this.$axios.get(
+        `${process.env.VUE_APP_WEBSOCKET}/alps/uploadOrder`,
+        { params: { socketId: this.$socket.id } }
+      );
+      this.loading = false;
+    }
   }
 };
 </script>
