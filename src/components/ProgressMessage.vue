ecf62543ffea4e267d89622029624002e8f13dff
aispark <aispark@paran.com>
2019-12-11 15:33:45 +0900

    프로퍼티 추가

--- src/components/ProgressMessage.vue
+++ src/components/ProgressMessage.vue
@@ -1,7 +1,7 @@
 <template>
-  <v-card>
-    <v-card-title class="headline">진행상황</v-card-title>
-    <v-card-text>
+  <v-card :loading="loading">
+    <v-card-title class="subheading font-weight-bold">진행상황</v-card-title>
+    <v-card-text v-show="isShow">
       <template v-for="(message, index) in items">
         <p :key="`${index}-progressMessage`">{{ message }}</p>
       </template>
@@ -15,6 +15,14 @@ export default {
     items: {
       type: Array,
       default: () => []
+    },
+    isShow: {
+      type: Boolean,
+      default: true
+    },
+    loading: {
+      type: Boolean,
+      default: false
     }
   },
   methods: {}
